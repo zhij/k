20ef36fa89cc5f4cd7c45f5be0d1be36721ce627
anzizhao <anzizhao@126.com>
2016-09-23 15:28:57 +0800

    保存修改内容

--- tpl/ares/build/.cache/aresScore.js
+++ tpl/ares/build/.cache/aresScore.js
@@ -1,14 +1,14 @@
-/*TMODJS:{"version":1,"md5":"260bc76116ec81998ec81c66c0b0dd29"}*/
+/*TMODJS:{"version":2,"md5":"f32555614321ceb6427582939cab2304"}*/
 template('aresScore',function($data,$filename
 /**/) {
-'use strict';var $utils=this,$helpers=$utils.$helpers,$escape=$utils.$escape,new_stores_num=$data.new_stores_num,heat=$data.heat,proportion=$data.proportion,beat_num=$data.beat_num,include=function(filename,data){data=data||$data;var text=$utils.$include(filename,data,$filename);$out+=text;return $out;},one_heat=$data.one_heat,two_heat=$data.two_heat,three_heat=$data.three_heat,$out='';$out+='<div> <a class="ico-home"></a> <div class="r-h1">拓展店数：<span class="r-h1-num">';
+'use strict';var $utils=this,$helpers=$utils.$helpers,$escape=$utils.$escape,new_stores_num=$data.new_stores_num,heat=$data.heat,proportion=$data.proportion,beat_ratio=$data.beat_ratio,include=function(filename,data){data=data||$data;var text=$utils.$include(filename,data,$filename);$out+=text;return $out;},one_heat=$data.one_heat,two_heat=$data.two_heat,three_heat=$data.three_heat,$out='';$out+='<div> <a class="ico-home"></a> <div class="r-h1">拓展店数：<span class="r-h1-num">';
 $out+=$escape(new_stores_num);
 $out+='间</span></div> <div class="r-h2"><span class="r-h2-num1 up">共';
 $out+=$escape(heat);
 $out+='°C</span><span class="r-h2-num2">占比';
 $out+=$escape(proportion);
 $out+='%</span></div> <div class="r-h3">击败全国<span class="r-h3-num">';
-$out+=$escape(beat_num);
+$out+=$escape(beat_ratio);
 $out+='%</span>的战神</div> ';
 include('./ares');
 $out+=' <div class="r-title">队内热度占比</div> <div class="circle-group"> <div class="circle-wp mr"> <div class="circle circle_1"> <div class="pie_left"><div class="left"></div></div> <div class="pie_right"><div class="right"></div></div> <div class="mask"><i>1°C</i><span>';
